5b552ba51320dbea16bf52b6f40d1cd245599237
Scholar Name <scholar@kodewithklossy.com>
2025-04-09 09:46:36 -0500

    Add navigation title

---
+++ Navigation/SecondView.swift
@@ -0,0 +1,21 @@
+//
+//  SecondView.swift
+//  Navigation
+//
+//  Created by Scholar on 4/9/25.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text("You've arrived to the second view!")
+            .foregroundColor(Color.purple)
+            .multilineTextAlignment(.center)
+        Text("😀")
+    }
+}
+
+#Preview {
+    SecondView()
+}
